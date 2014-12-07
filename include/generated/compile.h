428c8407166ee06fa292a5559f4f658794953899
santod <santod040@gmail.com>
2014-12-07 02:05:17 -0800

    Initial commit of HTC GPE source

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Dec 5 00:21:49 PST 2014"
+#define LINUX_COMPILE_BY "santod"
+#define LINUX_COMPILE_HOST "DevBoX"
+#define LINUX_COMPILER "gcc version 4.7.4 20140323 (prerelease) (SM-Toolchain) "
